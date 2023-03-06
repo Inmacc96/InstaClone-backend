1b01990d747ca5ec4b1c7a8b901eb667253bd12e
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-03-06 17:56:34 +0100

    Create User type in schema

--- src/gql/schema.ts
+++ src/gql/schema.ts
@@ -1,8 +1,14 @@
 const typeDefs = `#graphql
   type User {
-    id: ID
-    name: String
-    username: String
+    id: ID!
+    name: String!
+    username: String!
+    email:String!
+    avatar: String
+    siteWeb: String
+    description: String
+    password: String!
+    createdAt: String
   }
 
   type Query {
