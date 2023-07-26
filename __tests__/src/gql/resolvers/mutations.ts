947bc6942ea7a11e6b005c9955f3ad8a510c0fd4
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-07-26 19:16:31 +0200

    tests: add new tests for the authUser mutation

--- __tests__/src/gql/resolvers/mutations.ts
+++ __tests__/src/gql/resolvers/mutations.ts
@@ -1,14 +1,15 @@
 import { ApolloServer, BaseContext } from "@apollo/server";
 import { readFileSync } from "fs";
 import { GraphQLError } from "graphql";
-import { createUser } from "../../../../src/controllers/user";
+import { createUser, authUser } from "../../../../src/controllers/user";
 import resolvers from "../../../../src/gql/resolvers";
-import { UserInput, User } from "../../../../src/types/graphql";
+import { UserInput, User, AuthInput } from "../../../../src/types/graphql";
 
 const typeDefs = readFileSync("src/gql/schema.graphql", { encoding: "utf-8" });
 
 jest.mock("../../../../src/controllers/user", () => ({
   createUser: jest.fn(),
+  authUser: jest.fn(),
 }));
 
 let testServer: ApolloServer<BaseContext>;
@@ -26,7 +27,7 @@ describe("[Mutation.newUser]", () => {
       name
       email
       username
-      password
+      createdAt
       }
   }
 `;
@@ -164,6 +165,72 @@ describe("[Mutation.newUser]", () => {
       email: "usertest@usertest.com",
       password: "usertest123",
     };
+
+    const response = await testServer.executeOperation({
+      query,
+      variables: { input: userInput },
+    });
+
+    expect(response).toMatchSnapshot();
+  });
+});
+
+describe("[Mutation.authUser]", () => {
+  const query = `#graphql
+  mutation authUser($input: AuthInput!){
+    authUser(input: $input){
+      token
+    }
+}`;
+
+  it("should return a token when the input is correct", async () => {
+    const userInput: AuthInput = {
+      email: "usertest@usertest.com",
+      password: "usertest123",
+    };
+
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MDljOWNlZjIwZGI1NmRiMzVkZjRjZCIsImVtYWlsIjoidXNlcnRlc3RAdXNlcnRlc3QuY29tIiwidXNlcm5hbWUiOiJ1c2VydGVzdCIsIm5hbWUiOiJ1c2VyIHRlc3QiLCJpYXQiOjE2NzgzNjMwODYsImV4cCI6MTY3ODQ0OTQ4Nn0.5fXyYiF9bOzHJNvLzEWBspkQAN4V8qIPKOo10qciEg8";
+
+    (authUser as jest.Mock).mockResolvedValue({ token });
+
+    const response = await testServer.executeOperation({
+      query,
+      variables: { input: userInput },
+    });
+
+    expect(response).toMatchSnapshot();
+  });
+
+  it("should throw an error when wrong email are provided", async () => {
+    const userInput: AuthInput = {
+      email: "usertest2@usertest.com",
+      password: "usertest123",
+    };
+
+    const error = {
+      msg: "Wrong credentials",
+      code: "BAD_USER_INPUT",
+    };
+
+    (authUser as jest.Mock).mockRejectedValue(
+      new GraphQLError(error.msg, {
+        extensions: {
+          code: error.code,
+        },
+      })
+    );
+
+    const response = await testServer.executeOperation({
+      query,
+      variables: { input: userInput },
+    });
+
+    expect(response).toMatchSnapshot();
+  });
+
+  it("should throw error when the input is empty", async () => {
+    const userInput = {};
 
     const response = await testServer.executeOperation({
       query,
