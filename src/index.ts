fcb4f2800e8db87b45113629ffdf46ecfd20f8c9
inmacc96 <60236648+Inmacc96@users.noreply.github.com>
2023-09-08 21:01:01 +0200

    Config cloudinary

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { readFileSync } from "fs";
 import connectDB from "./config/db.config";
 import resolvers from "./gql/resolvers";
+import configCloudinary from "./config/cloudinary.config";
 
 const typeDefs = readFileSync("src/gql/schema.graphql", { encoding: "utf-8" });
 
 const startServer = async () => {
   // Database connection
   connectDB();
+
+  // Config cloudinary
+  configCloudinary();
 
   // Apollo server
   const server = new ApolloServer<BaseContext>({ typeDefs, resolvers });
